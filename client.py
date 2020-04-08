9f0c3de827df1c363051266b9fd11f2d49102cc4
jkelly01 <jennifer.kelly@vmsuk.com>
2020-04-08 19:53:26 +0100

    adding graphing feature for OHM results

--- client.py
+++ client.py
@@ -3,12 +3,17 @@
 import socket
 import json
 import tkinter as tk
+from tkinter import messagebox
 import threading
 import queue
 import time
+import matplotlib.pyplot as plt
+from os import path
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from matplotlib.figure import Figure
 
 queue_1 = queue.Queue(maxsize=1)  # queue for user instruction to send to server.py
-queue_2 = queue.Queue(maxsize=20)  # queue for received data to be added and to be accessed to write to json
+queue_2 = queue.Queue(maxsize=50)  # queue for received data to be added and to be accessed to write to json
 
 
 class SendInstructions(threading.Thread):
@@ -21,9 +26,10 @@ class SendInstructions(threading.Thread):
     def run(self):
         while True:
             data = queue_1.get()
+            # print(data)
             data = json.dumps(data)
             self.sock.sendto(bytes(str(data), "utf-8"), (self.UDP_IP, self.UDP_PORT))
-            print("request sent to server")
+            # print("request sent to server")
 
 
 class ReceiveData(threading.Thread):
@@ -32,20 +38,19 @@ class ReceiveData(threading.Thread):
         self.UDP_IP = "localhost"
         self.UDP_PORT = 5006
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.sock.settimeout(30)
+        self.sock.settimeout(120)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind((self.UDP_IP, self.UDP_PORT))
 
     def run(self):
-        print("receive_data: started")
+        # print("receive_data: started")
         x = 0
         try:
             for i in range(0, 100):
                 data, addr = self.sock.recvfrom(1024)
-                print(f'received data: {data}')
-                # data = data.decode()
+                # print(f'received data: {data}')
                 queue_2.put(data)
-                print("receive data placed in queue")
+                # print("receive data placed in queue")
                 x += 1
                 if x == int(instructions['num']):
                     break
@@ -58,73 +63,136 @@ class ReceiveData(threading.Thread):
 class WriteToJSON(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
-        self.results = {}
 
     def run(self):
-        # if not queue_2.empty():
-        # exception if no attributes were selected
-        print('json_thread: STARTED')
+        # print('json_thread: STARTED')
+
         with open('data.json', 'w') as file:
-            file.write("[")  # write a json array (list)
+
+            data_dict = {'Data': {}}
+
             for i in range(0, int(instructions['num'])):
                 data = queue_2.get()
                 data = str(data.decode("utf-8"))
-                # print(data)
-                # json_data = json.dumps(data) + '\n'
                 parsed = json.loads(data)
-                json.dump(parsed, file, indent=4, sort_keys=True, separators=(',', ': '))
-                if i < int(instructions['num'])-1:
-                    file.write(",")  # don't append comma on last entry
-                file.write("\n")
-            file.write("]")  # close the list
+                # data_list.append(parsed)
+                data_dict['Data'][i] = parsed
 
-        print('json_thread: ENDED')
+            json.dump(dict(data_dict), file, indent=4, sort_keys=True, separators=(',', ': '))
+
+        # print('json_thread: ENDED')
 
 
 class GraphicalUserInterface:
     def __init__(self):
         self.window = tk.Tk()
-        self.window.title("Open Hardware Monitor")
-        self.window.geometry('400x250')
-        self.instructions = {'temperature': False, 'load': False, 'fan': False}
+        self.instructions = {'temperature': False, 'load': False, 'num': 0}
         self.entry = None
+        self.clicked = None
+        self.data_entries = []
+        self.time_entries = []
 
     def instruction_collector(self):
-        label = tk.Label(master=self.window, text="Select the data you would like from OpenHardwareMonitor:")
-        label.pack()
+        self.window.title("Open Hardware Monitor")
+        self.window.geometry('500x300')
 
-        temp_button = tk.Button(command=self.button_click_temp, master=self.window, text='temperature')
-        temp_button.pack(side="top")
+        label_data = tk.Label(master=self.window, text="Select the data you would like from OpenHardwareMonitor:")
+        label_data.pack()
 
-        load_button = tk.Button(command=self.button_click_load, master=self.window, text='load')
-        load_button.pack(side="top")
+        self.clicked = tk.StringVar()
+        self.clicked.set("Temperature")
 
-        fan_button = tk.Button(command=self.button_click_fan, master=self.window, text='fan speed')
-        fan_button.pack(side="top")
+        options = tk.OptionMenu(self.window, self.clicked, "Temperature", "Load")
+        options.pack()
 
-        self.entry = tk.Entry()
+        label_num = tk.Label(master=self.window, text="Print the number of values you would like. (max=50)")
+        label_num.pack()
+
+        self.entry = tk.Entry(self.window)
         self.entry.pack()
+        callback = self.window.register(self.only_numeric_input)  # registers a Tcl to Python callback
+        self.entry.configure(validate="key", validatecommand=(callback, "%P"))  # enables validation
 
         tk.Button(master=self.window, text='get results', command=self.quit).pack(side='top')
 
         self.window.mainloop()
         return self.instructions
 
-    def button_click_temp(self):
-        self.instructions['temperature'] = True
+    def only_numeric_input(self, P):
+        if P.isdigit():
+            return True
+        return False
 
-    def button_click_load(self):
-        self.instructions['load'] = True
+    def get_instructions(self):
+        if self.clicked.get() == 'Temperature':
+            self.instructions['temperature'] = True
 
-    def button_click_fan(self):
-        self.instructions['fan'] = True
+        elif self.clicked.get() == 'Load':
+            self.instructions['load'] = True
 
-    def num_results(self):
         self.instructions['num'] = self.entry.get()
 
     def quit(self):
-        self.num_results()
+        self.get_instructions()
         self.window.destroy()
+
+    def graph(self):
+        self.data_entries, self.time_entries = self.data_handler()
+
+        x = self.time_entries
+        y = self.data_entries
+
+        plt.plot(x, y)
+
+        plt.xlabel('time (seconds)')
+        if self.instructions['temperature']:
+            plt.ylabel('temperature (celsius)')
+        else:
+            plt.ylabel('load (percentage)')
+
+        plt.title('Open Hardware Monitor Results')
+
+        plt.show()
+
+        '''
+        self.window = tk.Tk()
+        self.window.title('Open Hardware Monitor Results')
+
+        fig = Figure(figsize=(5, 4), dpi=100)
+        fig.add_subplot(111).plot(x, y)
+
+        canvas = FigureCanvasTkAgg(fig, master=self.window)
+        canvas.draw()
+
+        toolbar = NavigationToolbar2Tk(canvas, self.window)
+        toolbar.update()
+
+        button = tk.Button(master=self.window, text="Quit", command=self.quit_graph)
+        button.pack(side=tk.BOTTOM)
+
+        canvas.get_tk_widget().pack(side=self.window.TOP, fill=self.window.BOTH, expand=1)
+
+        self.window.mainloop()
+        '''
+
+    def quit_graph(self):
+        self.window.destroy()
+
+    def data_handler(self):
+        with open('data.json', 'r') as file:
+            file = file.read()
+            # print('dict: ' + str(file))
+            file = json.loads(file)
+            file = dict(file)
+
+            for i in range(int(instructions['num'])):
+                self.data_entries.append(int(file['Data'][str(i)]['Data_Value']))
+
+            for second in range(0, int(instructions['num'])):
+                self.time_entries.append(second)
+
+        # print('data list: ' + str(self.data_entries))
+        return self.data_entries, self.time_entries
 
 
 if __name__ == '__main__':
@@ -135,12 +203,18 @@ if __name__ == '__main__':
 
     instructions = gui.instruction_collector()
 
-    print(instructions)
-
     queue_1.put(instructions)
-    sender.start()
 
-    time.sleep(5)
+    sender.start()
+    time.sleep(2)
 
     receiver.start()
+
     json_writer.start()
+    json_writer.join()
+
+    gui.graph()
+
+    sender.join()
+    receiver.join()
+    exit()
--- server.py
+++ server.py
@@ -6,13 +6,11 @@ import wmi
 import threading
 import queue
 import json
-from client import ReceiveData
+import pythoncom
+import win32
 
 queue_1 = queue.Queue(maxsize=1)  # queue for received instructions to dictate OHM data
-queue_2 = queue.Queue(maxsize=20)  # queue for OHM data to be used when sending data
-hardware_monitor = wmi.WMI(namespace=r"root\OpenHardwareMonitor")
-
-# print(hardware_monitor.sensor())
+queue_2 = queue.Queue(maxsize=50)  # queue for OHM data to be used when sending data
 
 
 class ReceiveInstructions(threading.Thread):
@@ -27,17 +25,16 @@ class ReceiveInstructions(threading.Thread):
 
     def run(self):
         if not queue_1.full():
-            print("receive_data: started")
+            # print("receive_data: started")
             try:
                 data, addr = self.sock.recvfrom(1024)
-                print(f'received data: {data}')
-                # data = data.decode()
+                # print(f'received data: {data}')
                 queue_1.put(data)
-                print("receive data placed in queue")
+                # print("receive data placed in queue")
             except socket.timeout:
-                print("didn't receive any data (ReceiveData)")
+                # print("didn't receive any data (ReceiveData)")
                 self.sock.close()
-                print("receive socket closed")
+                # print("receive socket closed")
 
 
 class DataSender(threading.Thread):
@@ -50,13 +47,12 @@ class DataSender(threading.Thread):
 
     def run(self):
         while True:
-            # for i in range(int(instructions['num'])):
             if not queue_2.empty():
                 q_data = queue_2.get()
                 s_data = json.dumps(q_data)
-                print(f"sending data - {s_data}")
+                # print(f"sending data - {s_data}")
                 self.sock.sendto(bytes(str(s_data), "utf-8"), (self.UDP_IP, self.UDP_PORT))
-                time.sleep(1)
+                # time.sleep(1)
             if queue_2.empty():
                 break
 
@@ -65,26 +61,26 @@ class OpenHardwareMonitor(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
 
-    def run(self):
+    def collect_data(self):
+        pythoncom.CoInitialize()
         record_id = 1
 
         for i in range(int(instructions['num'])):
 
             data_results_temp = {}
             data_results_load = {}
-            data_results_fan = {}
 
             if instructions['temperature']:
                 sensors_temp = hardware_monitor.Sensor(SensorType="Temperature")
                 # print(sensors_temp)
                 for temperature in sensors_temp:
-                    if (temperature.Identifier.find("ram") == -1) and (temperature.Identifier.find("hdd") == -1) and (
-                            temperature.Name.find("Package") == -1):
-                        data_results_temp['Rec_Type'] = "Temp"
-                        data_results_temp['Time'] = time.time()
+                    if (temperature.Identifier.find("ram") == -1) and (
+                                temperature.Identifier.find("hdd") == -1) and (
+                                temperature.Name.find("Package") == -1):
+
                         data_results_temp['Record_ID'] = record_id
-                        sensor_name = temperature.Name
-                        data_results_temp[sensor_name] = temperature.value
+                        data_results_temp['Rec_Type'] = "Temp"
+                        data_results_temp['Data_Value'] = temperature.value
 
                 # only put an item on queue if it is populated
                 if data_results_temp:
@@ -95,43 +91,30 @@ class OpenHardwareMonitor(threading.Thread):
                 sensors_load = hardware_monitor.Sensor(SensorType="Load")
                 for load in sensors_load:
                     if (load.Identifier.find("ram") == -1) and (load.Identifier.find("hdd") == -1) and (
-                            load.Name.find("Total") == -1):
-                        data_results_load['Rec_Type'] = 'Load'
-                        data_results_load['Time'] = time.time()
-                        data_results_load['Record_ID'] = record_id
-                        sensor_name = load.name
-                        data_results_load[sensor_name] = load.value
+                                load.Name.find("Total") == -1):
+
+                        if load.name == 'CPU Core #1':
+                            data_results_load['Record_ID'] = record_id
+                            data_results_load['Rec_Type'] = "Load"
+                            data_results_load['Data_Value'] = load.value
 
                     if data_results_load:
                         print(f"adding to queue - {data_results_load}")
                         queue_2.put(data_results_load)
 
-            if instructions['fan']:
-                sensors_fan = hardware_monitor.Sensor(SensorType="Fan")
-                for fan in sensors_fan:
-                    if (fan.indentifier.find("ram") == -1) and (fan.identifier("hdd") == -1) and (
-                            fan.Name.find("Total") == -1):
-                        data_results_fan['Rec_Type'] = 'Fan Speed'
-                        data_results_fan['Time'] = time.time()
-                        data_results_fan['Record_ID'] = record_id
-                        sensor_name = fan.name
-                        data_results_fan[sensor_name] = fan.value
-
-                    if data_results_load:
-                        print(f"adding to queue - {data_results_fan}")
-                        queue_2.put(data_results_fan)
-
             time.sleep(1)
             record_id += 1
 
+        pythoncom.CoUnitialize()
+
 
 if __name__ == '__main__':
+    hardware_monitor = wmi.WMI(namespace=r"root\OpenHardwareMonitor")
     receiver = ReceiveInstructions()
     OHM = OpenHardwareMonitor()
     sender = DataSender()
     receiver.start()
     instructions = queue_1.get()
     instructions = json.loads(instructions)
-    time.sleep(3)
-    OHM.run()
+    OHM.collect_data()
     sender.start()
